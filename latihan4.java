5719c9d1170ccaa33574ce1b066ccffc924aee7b
Farhan020609 <farhansofianto0206@gmail.com>
2025-01-22 11:45:41 +0700

    adding latihan4

---
+++ latihan4.java
@@ -0,0 +1,28 @@
+package com.belajar;
+import java.util.Scanner;
+public class latihan4 {
+    public static void main(String[] args) {
+        // mengolah input data sederhana 
+        // menggunakan class scanner
+        int NISN, Usia;
+        String Nama, Alamat;
+        Scanner Input = new Scanner(System.in);
+        System.out.println("DATA SISWA");
+        System.out.println("----------");
+        System.out.print("NISN : ");
+        NISN = Input.nextInt();
+        System.out.print("Nama : ");
+        Nama = Input.next();
+        System.out.print("Alamat : ");
+        Alamat = Input.next();
+        System.out.print("Usia : ");
+        Usia = Input.nextInt();
+        // menampilkan data siswa 
+        System.out.println("DATA SISWA");
+        System.out.println("----------");
+        System.out.println("NISN : " + NISN);
+        System.out.println("Nama : " + Nama);
+        System.out.println("Alamat : " + Alamat);
+        System.out.println("Usia : " + Usia);
+    }
+}
