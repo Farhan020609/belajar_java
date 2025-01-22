5ba3bbd8db5852f8be93d8e4981007f72911f5f9
Farhan020609 <farhansofianto0206@gmail.com>
2025-01-22 11:56:15 +0700

    latihan1java

---
+++ latihan1.java
@@ -0,0 +1,15 @@
+package com.belajar;
+
+public class latihan1 {
+    public static void main(String[] args) { 
+        String namaSaya = "farhan sofianto";
+        String kelas = "x pplg 1";
+        double beratBadan = 49.00;
+        System.out.println("nama saya =" +" "+ namaSaya);
+        System.out.println("kelas saya ="+" " + kelas);
+        System.out.print("berat badan saya : ");
+        System.out.println(beratBadan);
+    }
+     
+ }
+
