dfdb62e814a6db0d6065b5db21085511c8c96ca2
Farhan020609 <farhansofianto0206@gmail.com>
2025-02-19 13:53:42 +0700

    luas lingkaran

---
+++ luaslingkaran.java
@@ -0,0 +1,53 @@
+package com.belajar;
+
+import java.util.Scanner;
+
+public class luaslingkaran {
+     public static void main(String[] args) {
+        luaslingkaran lingkaran = new luaslingkaran();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int pilihan;
+
+            do {
+                System.out.println("1. luas lingkaran");
+                System.out.println("2. keliling lingkaran");
+                System.out.println("3. keluar");
+                System.out.println("pilih menu");
+                pilihan = scanner.nextInt();
+
+                switch (pilihan) {
+                    case 1:
+                        lingkaran.hitungLuasLingkaran(scanner);
+                        break;
+                    case 2:
+                        lingkaran.hitungKelilingLingkaran(scanner);
+                        break;
+                    case 3:
+                        System.out.println("terima kasih");
+                        break;
+                    default:
+                        System.out.println("pilihan tidak ada");
+                        break;
+                }
+                System.out.println();
+            } while (pilihan != 3);
+        }
+    }
+
+    int jari, diameter;
+    double phi = 3.14;
+
+    void hitungLuasLingkaran(Scanner scanner) {
+        System.out.print("Masukkan jari-jari lingkaran untuk menghitung luas: ");
+        int jariLuas = scanner.nextInt();
+        double luas = phi * jariLuas * jariLuas;
+        System.out.println("Luas lingkaran adalah: " + luas);
+    }
+
+    void hitungKelilingLingkaran(Scanner scanner) {
+        System.out.print("Masukkan diameter lingkaran untuk menghitung keliling: ");
+        int diameterKeliling = scanner.nextInt();
+        double keliling = phi * diameterKeliling;
+        System.out.println("Keliling lingkaran adalah: " + keliling);
+    }
+}
